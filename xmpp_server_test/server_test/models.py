d14add09ba38df5cf7f3dba1a17901162d296571
Mathias Ertl <mati@er.tl>
2015-08-28 20:50:36 +0200

    start implementing some tests

--- xmpp_server_test/server_test/models.py
+++ xmpp_server_test/server_test/models.py
@@ -14,6 +14,8 @@
 # You should have received a copy of the GNU General Public License along with
 # django-xmpp-server-test.  If not, see <http://www.gnu.org/licenses/>.
 
+from dns import resolver
+
 from django.db import models
 
 from jsonfield import JSONField
@@ -32,6 +34,65 @@ class ServerTest(models.Model):
     modified = models.DateTimeField(auto_now=True)
     finished = models.BooleanField(default=False)
     data = JSONField(default={}, blank=True)
+
+    def srv_lookup(self, kind, domain):
+        key = '%s_records' % kind
+        try:
+            for record in resolver.query('_xmpp-%s._tcp.%s' % (kind, domain), 'SRV'):
+                host = record.target.split(1)[0].to_text()
+
+                # do A/AAAA lookup
+                ips = []
+                try:
+                    for a in resolver.query(host, 'A'):
+                        ips.append(a.address)
+                except resolver.NXDOMAIN:
+                    self.data['dns']['ipv4'] = False
+                try:
+                    for aaaa in resolver.query(host, 'AAAA'):
+                        ips.append(aaaa.address)
+                except resolver.NXDOMAIN:
+                    self.data['dns']['ipv6'] = False
+
+                self.data['dns'][key].append({
+                    'port': record.port,
+                    'host': host,
+                    'ips': ips,
+                })
+
+            # check if there is at least one record pointing to at least one IP
+            validity = [bool(e['ips']) for e in self.data['dns'][key]]
+            if True not in validity:
+                self.data['dns'][kind] = False
+        except resolver.NoAnswer:
+            self.data['dns']['srv'] = False
+            self.data['dns'][kind] = False
+
+    def start_test(self):
+        server = self.server
+        domain = server.domain
+
+        self.data['dns'] = {
+            'client_records': [],
+            'client': True,
+            'ipv4': True,
+            'ipv6': True,
+            'server_records': [],
+            'server': True,
+            'srv': True,
+        }
+
+        # Do client SRV lookups
+        self.srv_lookup('client', domain)
+        self.srv_lookup('server', domain)
+
+        if self.data['dns']['srv'] is False or self.data['dns']['client'] is False or \
+                self.data['dns']['server'] is False:
+            self.save()
+            return  # no SRV records
+
+        self.finished = True
+        self.save()
 
     def __str__(self):
         return '%s: %s' % (self.server.domain, self.created.strftime('%Y-%m-%d %H:%M'))
