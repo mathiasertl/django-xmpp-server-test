2c77061af3285af8eeac4a21e5c853e8a48aa5cb
Mathias Ertl <mati@er.tl>
2015-08-28 18:50:25 +0200

    add bare non-functioning views

--- xmpp_server_test/server_test/views.py
+++ xmpp_server_test/server_test/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+#
+# This file is part of django-xmpp-server-test
+# (https://github.com/mathiasertl/django-xmpp-server-test).
+#
+# django-xmpp-server-test is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software Foundation, either
+# version 3 of the License, or (at your option) any later version.
+#
+# django-xmpp-server-test is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# django-xmpp-server-test.  If not, see <http://www.gnu.org/licenses/>.
 
-# Create your views here.
+from django.views.generic import ListView
+from django.views.generic import DetailView
+
+from .models import Server
+from .models import ServerTest
+
+
+class RootView(ListView):
+    queryset = Server
+
+
+class ServerView(DetailView):
+    pass
+
+
+class ServerTestView(DetailView):
+    pass
