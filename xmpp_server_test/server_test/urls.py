24d3b38155385520cd99adcc72556c6f0fa5f58d
Mathias Ertl <mati@er.tl>
2015-08-28 18:50:36 +0200

    add urlconfg for app

---
+++ xmpp_server_test/server_test/urls.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of django-xmpp-server-test
+# (https://github.com/mathiasertl/django-xmpp-server-test).
+#
+# django-xmpp-server-test is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software Foundation, either
+# version 3 of the License, or (at your option) any later version.
+#
+# django-xmpp-server-test is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# django-xmpp-server-test.  If not, see <http://www.gnu.org/licenses/>.
+
+from django.conf.urls import url
+
+from . import views
+
+
+urlpatterns = [
+    url(r'^$', views.RootView.as_view(), name='overview'),
+    url(r'^(?P<domain>[\w-]+)/$', views.ServerView.as_view(), name='overview'),
+    url(r'^(?P<domain>[\w-]+)/(?P<pk>\d+)/$', views.ServerTestView.as_view(), name='overview'),
+]
