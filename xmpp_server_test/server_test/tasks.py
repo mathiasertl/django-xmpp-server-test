dbb019ed7c5df4523418ebe090893e07e0c40f51
Mathias Ertl <mati@er.tl>
2015-08-30 20:56:31 +0200

    add a bare test task

---
+++ xmpp_server_test/server_test/tasks.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of django-xmpp-server-test
+# (https://github.com/mathiasertl/django-xmpp-server-test).
+#
+# django-xmpp-server-test is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software Foundation, either
+# version 3 of the License, or (at your option) any later version.
+#
+# django-xmpp-server-test is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# django-xmpp-server-test.  If not, see <http://www.gnu.org/licenses/>.
+
+from celery import shared_task
+
+
+@shared_task
+def test():
+    print('print')
