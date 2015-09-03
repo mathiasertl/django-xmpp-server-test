536e42e41cf13f8ee9a1a95f0e0a4b1071a82e58
Mathias Ertl <mati@er.tl>
2015-09-03 13:09:20 +0200

    add status templatetag

---
+++ xmpp_server_test/server_test/templatetags/server_test.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of django-xmpp-server-test
+# (https://github.com/mathiasertl/django-xmpp-server-test).
+#
+# django-xmpp-server-test is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software Foundation, either
+# version 3 of the License, or (at your option) any later version.
+#
+# django-xmpp-server-test is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# django-xmpp-server-test.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from django import template
+from django.utils.safestring import mark_safe
+from django.utils.translation import ugettext as _
+
+register = template.Library()
+
+
+@register.filter
+def status(value):
+    text = '<span class="label '
+    if value is None:
+        text += 'label-default">' + _('Pending') + '</span>'
+    elif value is True:
+        text += 'label-success">' + _('OK') + '</span>'
+    elif value is False:
+        text += 'label-danger">' + _('Failed') + '</span>'
+    else:
+        text += 'label-danger">' + _('Unknown') + '</span>'
+    return mark_safe(text)
