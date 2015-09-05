b2f6ea4ab3ca818aafb7af4f0af1226cdabd43bd
Mathias Ertl <mati@er.tl>
2015-09-05 13:33:01 +0200

    add username/password fields

--- xmpp_server_test/server_test/forms.py
+++ xmpp_server_test/server_test/forms.py
@@ -19,3 +19,5 @@ from django import forms
 
 class DomainForm(forms.Form):
     domain = forms.CharField()
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput())
