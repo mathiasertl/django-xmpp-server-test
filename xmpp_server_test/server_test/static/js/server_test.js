9f05ea72031890c5c68a3c4b3d9be951cb60265a
Mathias Ertl <mati@er.tl>
2015-09-18 18:16:51 +0200

    cleanup

--- xmpp_server_test/server_test/static/js/server_test.js
+++ xmpp_server_test/server_test/static/js/server_test.js
@@ -1,15 +1,12 @@
 var refresh_test = function() {
     var details = $('#servertest-details');
     if (details.hasClass('in-progress')) {
-        console.log('Refreshing test...');
         $.get(refresh_url, function(data) {
             details.replaceWith(data);
         });
         if ($('#servertest-details').hasClass('in-progress')) {
             window.setTimeout(refresh_test, 3000);
         }
-    } else {
-        console.log('Test is already finished.');
     }
 }
 
