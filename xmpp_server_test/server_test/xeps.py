a35b453618198c7aab1920d999e825ce7d4e39d6
Mathias Ertl <mati@er.tl>
2015-09-20 20:23:19 +0200

    move everything into a common dictionary

--- xmpp_server_test/server_test/xeps.py
+++ xmpp_server_test/server_test/xeps.py
@@ -14,28 +14,61 @@
 # You should have received a copy of the GNU General Public License along with
 # django-xmpp-server-test.  If not, see <http://www.gnu.org/licenses/>.
 
-xep_names = {
-    '0012': 'Last Activity',
-    '0016': 'Privacy Lists',
-    '0030': 'Service Discovery',
-    '0039': 'Statistics Gathering',
-    '0050': 'Ad-Hoc Commands',
-    '0054': 'vcard-temp',
-    '0060': 'Publish-Subscribe',
-    '0077': 'In-Band Registration',
-    '0078': 'Non-SASL Authentication',
-    '0079': 'Advanced Message Processing',
-    '0092': 'Software Version',
-    '0115': 'Entity Capabilities',
-    '0138': 'Stream Compression',
-    '0160': 'Best Practices for Handling Offline Messages',
-    '0191': 'Blocking Command',
-    '0198': 'Stream Management',
-    '0199': 'XMPP Ping',
-    '0202': 'Entity Time',
-    '0220': 'Server Dialback',
-    '0280': 'Message Carbons',
-    '0288': 'Bidirectional Server-to-Server Connections',
-    '0313': 'Message Archive Management',
-    '0352': 'Client State Indication',
-}
+from collections import defaultdict
+
+from django.utils.translation import ugettext as _
+
+xeps = defaultdict(lambda: defaultdict(lambda: None))
+
+
+def xep0138_notes(value):
+    if value['status'] is False:
+        return ''
+
+    client = set(value.get('client', []))
+    server = set(value.get('server', []))
+
+    if value['status'] == 'partial':
+        if client:
+            return _('Only supported on client: %s') % ', '.join(sorted(client))
+        elif server:
+            return _('Only supported on client: %s') % ', '.join(sorted(server))
+        else:
+            return _('')
+
+    if client or server:  # server supports some methods somewhere
+        if client == server:  # same methods client/server
+            return _('Methods: %s') % ', '.join(sorted(client))
+        else:
+            label = '<span class="label label-warning">%s</span> %s' % (
+                _('Inconsistent methods:'), _('Warning'))
+            client = _('<li>Client: %s</li>') % ', '.join(sorted(client))
+            server = _('<li>Server: %s</li>') % ', '.join(sorted(server))
+            return '%s<ul>%s%s</ul>' % (label, client, server)
+
+    return ''
+
+xeps['0012']['name'] = 'Last Activity'
+xeps['0016']['name'] = 'Privacy Lists'
+xeps['0030']['name'] = 'Service Discovery'
+xeps['0039']['name'] = 'Statistics Gathering'
+xeps['0050']['name'] = 'Ad-Hoc Commands'
+xeps['0054']['name'] = 'vcard-temp'
+xeps['0060']['name'] = 'Publish-Subscribe'
+xeps['0077']['name'] = 'In-Band Registration'
+xeps['0078']['name'] = 'Non-SASL Authentication'
+xeps['0079']['name'] = 'Advanced Message Processing'
+xeps['0092']['name'] = 'Software Version'
+xeps['0115']['name'] = 'Entity Capabilities'
+xeps['0138']['name'] = 'Stream Compression'
+xeps['0138']['notes'] = xep0138_notes
+xeps['0160']['name'] = 'Best Practices for Handling Offline Messages'
+xeps['0191']['name'] = 'Blocking Command'
+xeps['0198']['name'] = 'Stream Management'
+xeps['0199']['name'] = 'XMPP Ping'
+xeps['0202']['name'] = 'Entity Time'
+xeps['0220']['name'] = 'Server Dialback'
+xeps['0280']['name'] = 'Message Carbons'
+xeps['0288']['name'] = 'Bidirectional Server-to-Server Connections'
+xeps['0313']['name'] = 'Message Archive Management'
+xeps['0352']['name'] = 'Client State Indication'
